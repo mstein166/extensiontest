03986014ab6a545121fd250d24d265002b9d96ce
mstein166 <53192833+mstein166@users.noreply.github.com>
2024-03-26 22:12:58 -0700

    Update content.js

--- content.js
+++ content.js
@@ -3,5 +3,5 @@ const title = document.querySelector("title");
 if (title) {
   const badge = document.createElement("p");
   badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `Your movie is ${title}!`;
+  badge.textContent = `Your movie is ${title.TextContent}!`;
 }

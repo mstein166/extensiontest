fd17d0eb7b5518079819762954516d26c898678f
mstein166 <53192833+mstein166@users.noreply.github.com>
2024-03-26 22:43:59 -0700

    Update content.js

--- scripts2/content.js
+++ scripts2/content.js
@@ -1,4 +1,4 @@
-const title = document.querySelector('h1.headline-1');
+const title = document.querySelector('h1.headline-1.js-widont.prettify');
 
 if (title) {
   const badge = document.createElement("p");

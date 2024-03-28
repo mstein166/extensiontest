392f712f9260e88347a24493bcb61f11adbb4012
mstein166 <53192833+mstein166@users.noreply.github.com>
2024-03-27 21:39:33 -0700

    Update content.js

--- scripts2/content.js
+++ scripts2/content.js
@@ -3,7 +3,10 @@ const title = document.querySelector('h1.headline-1');
 if (title !== null) {
         // This will get the text, including converting "&nbsp;" to a regular space
         const text = title.textContent;
-  
+        const displayElement = document.querySelector('#titleDisplay');
+        if (displayElement !== null) {
+            displayElement.textContent = text;
+        }
         // Print the text to the console
         console.log(text);
     } else {
